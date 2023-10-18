2c29835077ff8cc7144860042d8c4ecf3bd42e54
ZED <67244328+nickravesh@users.noreply.github.com>
2023-10-18 17:10:58 +0330

    [feat]: Implement task completion toggling in TaskWarden

--- TaskWarden.py
+++ TaskWarden.py
@@ -2,10 +2,16 @@ import utils
 import argparse
 
 parser = argparse.ArgumentParser(description="TaskWarden, A ToDo-List for those who love the command line.")
-parser.add_argument("taskTitle", help="the title of task")
-
+parser.add_argument("taskTitle", type=str, nargs="?", help="the title of task")
+parser.add_argument("-d", "--done", action="store_true", help="mark task as completed/uncompleted")
 args = parser.parse_args()
 
-userTask = args.taskTitle
-
-utils.addTask(userTask)
+if args.done and args.done:
+    # If -d is provided along with a taskTitle, mark the task as completed
+    utils.updateTaskCompletion(args.taskTitle, True)
+elif args.taskTitle:
+    # If only a taskTitle is provided, add the task
+    utils.addTask(args.taskTitle)
+else:
+    # Handle the case where no arguments are provided
+    parser.print_help()
--- utils.py
+++ utils.py
@@ -27,3 +27,73 @@ def addTask(taskTitle: str, isCompleted=False):
         json.dump(tasks, file, indent=4)
 
     print(f"{Fore.LIGHTGREEN_EX}-Task Added-")
+
+
+def updateTaskCompletion(taskTitle: str, isCompleted: bool):
+    # check for existence of tasks.json
+    if os.path.isfile("tasks.json") == False:
+        print(f"{Fore.LIGHTRED_EX}-No task to mark, you must first add a task-")
+        print(f"check the database")
+        return exit()
+    
+    if isCompleted:
+        tasks = []
+
+        try: # read/load the tasks from json into a variable
+            with open("tasks.json", "r") as file:
+                tasks = json.load(file)
+        except json.JSONDecodeError:
+            print(f"{Fore.LIGHTRED_EX}-No task to mark, you must first add a task-")
+            return exit()
+        except Exception as e:
+            print(f"{Fore.LIGHTRED_EX}An error occurred while loading tasks from JSON:{Fore.RESET}\n{e}")
+
+        # mark the task as completed
+        for task in tasks:
+            if task["title"] == taskTitle and task["completed"] == False:
+                task["completed"] = True
+
+                # write the updated task list to the json file
+                with open("tasks.json", "w") as file:
+                    json.dump(tasks, file, indent=4)
+
+                print(f"{Fore.LIGHTGREEN_EX}-Task marked as completed-")
+                return
+            
+            elif task["title"] == taskTitle and task["completed"] == True:
+                print(f"{Fore.LIGHTYELLOW_EX}-The task is already marked as completed-")
+                break
+        else:
+            print(f"{Fore.LIGHTRED_EX}-No task exists with the given name-")
+    
+
+    elif isCompleted == False:
+        try: # read/load the tasks from json into a variable
+            with open("tasks.json", "r") as file:
+                tasks = json.load(file)
+        except json.JSONDecodeError:
+            print(f"{Fore.LIGHTRED_EX}-No task to mark, you must first add a task-")
+            return exit()
+        except Exception as e:
+            print(f"{Fore.LIGHTRED_EX}An error occurred while loading tasks from JSON:{Fore.RESET}\n{e}")
+
+        # mark the task as uncompleted
+        for task in tasks:
+            if task["title"] == taskTitle and task["completed"] == True:
+                task["completed"] = False
+
+                # write the updated task list to the json file
+                with open("tasks.json", "w") as file:
+                    json.dump(tasks, file, indent=4)
+
+                print(f"{Fore.LIGHTYELLOW_EX}-Task marked as {Fore.LIGHTRED_EX}un{Fore.RESET}{Fore.LIGHTYELLOW_EX}completed-")
+                return
+            
+            elif task["title"] == taskTitle and task["completed"] == False:
+                print(f"{Fore.LIGHTYELLOW_EX}-The task is already marked as uncompleted-")
+                break
+        else:
+            print(f"{Fore.LIGHTRED_EX}-No task exists with the given name-")
+
+
+#updateTaskCompletion("task4", isCompleted=False)
